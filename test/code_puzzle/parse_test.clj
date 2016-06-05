6955a2ffcb5cfcfd3c71a530de7a400f8947e7d5
Denise Zimmerman <west.zimmerman@me.com>
2016-06-05 10:06:06 -0600

    create tests for parsing fns and add sample files

---
+++ test/code_puzzle/parse_test.clj
@@ -0,0 +1,66 @@
+(ns code-puzzle.parse-test
+  (:require [code-puzzle.parse :refer :all]
+            [code-puzzle.service :refer :all]
+            [clojure.string :as str]
+            [clojure.test :refer :all]))
+
+(def test-csv "resources/test.csv")
+(def test-psv "resources/test.psv")
+
+(def test-amt-col [:price-cents :discount-cents :price-dollarss
+                   :discount-dollars])
+(def test-num-col [:order-id])
+
+(deftest kebab-keyword-test
+  (testing "->kebab-keyword fn converts a string to a kebab-case keyword"
+    (let [unparsed-string "lemons lemurs lions"
+          converted-string :lemons-lemurs-lions]
+      (is (= (->kebab-keyword unparsed-string) converted-string)))))
+
+(deftest parse-csv-test
+  (testing "split-data separator"
+    (with-redefs [amount-columns test-amt-col
+                  num-columns test-num-col]
+      (let [csv->data-interim (-> test-csv
+                                  slurp
+                                  str/lower-case
+                                  str/split-lines
+                                  (split-data #",")) ; not testing these clj fns
+            parsed-data {:order-id "21133005",
+                         :price-cents "11050",
+                         :discount-cents "1000",
+                         :session-type "control"}
+            parsed-file (parse-file test-csv #",")
+            converted-to-dollars (cents->dollars
+                                  parsed-file
+                                  [:price-cents :discount-cents]
+                                  [:price-dollars :discount-dollars])]
+        (is (= (first (parse-data csv->data-interim)) parsed-data))
+        (testing "conversion fn returns the expected class for the values"
+          (instance? String (:session-type (first parsed-file)))
+          (instance? Integer (:order-id (first parsed-file)))
+          (instance? BigDecimal (:price-cents (first parsed-file))))
+        (testing "cents keys and values are converted to dollars"
+          (is (= 110.5 ((first converted-to-dollars) :price-dollars))))
+        (testing "data no longer contains cents keys"
+          (is (false? (.contains [:price-cents :discount-cents]
+                                 (first converted-to-dollars)))))))))
+(deftest parse-psv-test
+  (testing "parse-file function can handle psv file as input"
+    (with-redefs [amount-columns test-amt-col
+                  num-columns test-num-col]
+      (let [psv->data-interim (-> test-psv
+                                  slurp
+                                  str/lower-case
+                                  str/split-lines
+                                  (split-data #"\|"))
+            parsed-data {:order-id "21133005",
+                         :price-dollars "110.0",
+                         :discount-dollars "20.5",
+                         :session-type "control"}
+            parsed-file (parse-file test-psv #"\|")]
+        (is (= (first (parse-data psv->data-interim)) parsed-data))
+        (testing "conversion fn returns the expected class for the values"
+          (instance? String (:session-type (first parsed-file)))
+          (instance? Integer (:order-id (first parsed-file)))
+          (instance? BigDecimal (:price-cents (first parsed-file))))))))
